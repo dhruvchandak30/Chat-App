453bd3afcd4ad0796b6e14f6b30dec1cb0d335d9
Dhruv Chandak <dhruvchandak5@gmail.com>
2023-11-11 17:11:14 +0530

    Established Connection to Socket.IO

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
 
 const publicPath = path.join(__dirname, "/../public");
 console.log(publicPath);
 const port = process.env.PORT || 3000;
 
-var app = express();
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("A new User Connected");
+});
+
+server.listen(port, () => {
   console.log(`Server Started on Port ${port}`);
 });
